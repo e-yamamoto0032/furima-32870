d718f85eca566f9c090b854d6f5905d0ef2807a3
ettyan <eyama0032@gmail.com>
2021-01-26 19:43:51 +0900

    購入画面エラーメッセージ表示まで

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,8 +1,35 @@
 class RecordsController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @record_address = RecordAddress.new
-    
+  end
+
+  def create
+    @record_address = RecordAddress.new(address_params)
+    if @record_address.valid?
+     # Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+     # Payjp::Charge.create(
+     # amount: card_params[:price], 
+     # card: card_params[:token], 
+     # currency: 'jpy'
+     # )
+      @record_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:record_address).permit(
+                                    :postcode, :prefecture_id, 
+                                    :city, :block, :building, 
+                                    :phone_number, :record_id
+                                  ).merge(user_id: current_user.id) # token: params[:token])
   end
 
 end
--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -2,7 +2,7 @@ class RecordAddress
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, 
                 :city, :block, :building, 
-                :phone_number
+                :phone_number, :record_id, :user_id #:token
 
   with_options presence: true do
     validates :postcode, format: { with:/\A\d{3}[-]\d{4}\z/ }
@@ -10,6 +10,15 @@ class RecordAddress
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+   # validates :token, presence: true
+
+  def save
+    record = Record.create(item_id: item_id,user_id: user_id )
+
+    Address.create(postcode: postcode, prefecture_id: prefecture_id,
+                   city: city, block: block, building: building,
+                   phone_number: phone_number, record_id: record_id)
   end
 
 end
